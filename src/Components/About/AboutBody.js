5e317cd38fd63548425f672feedf1bccf2853811
Aristaeus <antoniojoccclaro@gmail.com>
2021-12-21 20:41:40 +0000

    about changes, img import, etc

--- src/Components/About/AboutBody.js
+++ src/Components/About/AboutBody.js
@@ -1,44 +1,24 @@
 import React from 'react';
-import './aboutbody.css';
+import './about.css';
+import ImageAbout1 from './img/image-about1.png';
+import ImageAbout2 from './img/image-about2.png';
 
 function AboutBody() {
     
     return (
         <>
-            <p>Esta é a página da biografia do Paulo Lisboa</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-            ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet porttitor eget. 
-            Fermentum iaculis eu non diam phasellus vestibulum lorem. Congue nisi vitae suscipit tellus. 
-            Metus dictum at tempor commodo ullamcorper a. Dignissim enim sit amet venenatis urna cursus 
-            eget nunc. Suspendisse faucibus interdum posuere lorem ipsum dolor. Porttitor leo a diam 
-            sollicitudin tempor id. Erat pellentesque adipiscing commodo elit at imperdiet. Integer eget 
-            aliquet nibh praesent. Elementum sagittis vitae et leo duis. Quam viverra orci sagittis eu. 
-            Scelerisque purus semper eget duis at tellus at urna. Nibh venenatis cras sed felis eget velit 
-            aliquet. Sed enim ut sem viverra aliquet eget.
-            Egestas maecenas pharetra convallis posuere morbi leo. Massa enim nec dui nunc. Lectus sit amet 
-            est placerat in egestas erat imperdiet. Eu lobortis elementum nibh tellus molestie. Tortor 
-            dignissim convallis aenean et tortor at risus. Sollicitudin nibh sit amet commodo nulla facilisi 
-            nullam vehicula. Facilisis leo vel fringilla est ullamcorper. Eleifend quam adipiscing vitae proin. 
-            Metus dictum at tempor commodo ullamcorper a lacus vestibulum. Pellentesque dignissim enim sit amet 
-            venenatis urna. Diam vel quam elementum pulvinar etiam non. Ut lectus arcu bibendum at varius vel. 
-            Duis at consectetur lorem donec. Placerat vestibulum lectus mauris ultrices eros in.
-            Et ligula ullamcorper malesuada proin libero nunc. Id ornare arcu odio ut sem nulla. Sed 
-            euismod nisi porta lorem mollis aliquam ut porttitor leo. Purus non enim praesent elementum 
-            facilisis leo vel fringilla. Quisque sagittis purus sit amet volutpat consequat. Id semper 
-            risus in hendrerit gravida rutrum. Eget dolor morbi non arcu risus quis varius. Ornare quam 
-            viverra orci sagittis eu volutpat odio facilisis. Id donec ultrices tincidunt arcu non sodales 
-            neque sodales ut. Sit amet tellus cras adipiscing enim eu turpis egestas. Egestas egestas 
-            fringilla phasellus faucibus. Tincidunt tortor aliquam nulla facilisi cras.
-            Lacinia at quis risus sed vulputate odio ut enim. Lacus vestibulum sed arcu non odio euismod. 
-            Vel turpis nunc eget lorem. Consectetur adipiscing elit ut aliquam purus sit amet luctus. 
-            Scelerisque felis imperdiet proin fermentum leo vel orci porta non. Duis ut diam quam nulla 
-            porttitor. Felis eget velit aliquet sagittis id consectetur purus ut. Velit aliquet sagittis 
-            id consectetur purus ut faucibus. In eu mi bibendum neque egestas. Consectetur adipiscing elit
-             ut aliquam. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. 
-             Et pharetra pharetra massa massa ultricies mi quis. Nulla malesuada pellentesque elit eget. 
-             Sit amet tellus cras adipiscing enim eu turpis egestas. A diam sollicitudin tempor id eu nisl. 
-             Euismod nisi porta lorem mollis aliquam ut porttitor leo. Vulputate enim nulla aliquet porttitor
-              lacus luctus. Ut sem nulla pharetra diam sit amet nisl suscipit adipiscing.</p>
+            <div className="img-about-wraper">
+                <div className="img-about1">
+                    <img src={ImageAbout1} />
+                </div>
+                <div className="img-about1-2">
+                    <img src={ImageAbout1} />
+                </div>
+                <div className="img-about2">
+                    <img src={ImageAbout2} />
+                </div>
+            </div>    
+            
         </>
     )
 
