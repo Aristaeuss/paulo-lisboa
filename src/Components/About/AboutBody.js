60d9cf9ac97f4148d63554a8ddfe8f4f10dfe148
Aristaeus <antoniojoccclaro@gmail.com>
2022-01-03 22:08:57 +0000

    about changings

--- src/Components/About/AboutBody.js
+++ src/Components/About/AboutBody.js
@@ -11,20 +11,18 @@ function AboutBody() {
     return (
         <>
             <div className="row">
-                <div className="img-wrapper">
+                <div className="images">
                     <div className="img-about1">
                         <img src={ImageAbout1} />
                     </div>
                     <div className="img-about2">
                         <img src={ImageAbout2} />
                     </div>
+                    {/*<span>"{t('Texto_Sobre.1')}"</span>
+                    <span>"{t('Texto_Sobre.2')}"</span>*/}
                 </div>
-                <p>"{t('Texto_Sobre.1')}"</p>
             </div>    
-            <div className="text-about-wrapper">
-                <p>"{t('Texto_Sobre.2')}"</p>
-            </div>
-            <div className="text-about-wrapper">
+            {/* <div className="text-about-wrapper">
                 <p>"{t('Texto_Sobre.3')}"</p>
             </div>
             <div className="text-about-wrapper">
@@ -43,8 +41,8 @@ function AboutBody() {
                 <p><i>{t('Texto_Sobre.titulo')}</i></p>
             </div>
             <div className="text-about-wrapper">
-                <p>{t('Texto_Sobre.autor')}</p>
-            </div>
+                <p><b>{t('Texto_Sobre.autor')}</b></p>
+            </div> */}
         </>
     )
 
